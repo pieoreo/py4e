c2aeb5261826cc07c227f8a7a1ecb605d4de946d
pieoreo <72796740+pieoreo@users.noreply.github.com>
2022-08-21 08:51:30 +0900

    Add files via upload

---
+++ 30th.py
@@ -0,0 +1,6 @@
+counts = dict()
+line = 'The general pattern to count the words in a line of text is to split the line into words, then loop through the words and use a dictionary to track the count of each word independently.'
+
+words = line.split()
+
+print('Words: ', words)
