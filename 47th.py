cc60c1e2b28b918ca97a8fc32585fd541ca62524
pieoreo <72796740+pieoreo@users.noreply.github.com>
2022-08-22 07:51:39 +0900

    Add files via upload

---
+++ 47th.py
@@ -0,0 +1,18 @@
+x = (3, 2, 1)
+#x.sort()
+#Traceback (most recent call last):
+#  File "c:\Users\JAMES\Desktop\py4e\47th.py", line 2, in <module>
+#    x.sort()
+#AttributeError: 'tuple' object has no attribute 'sort'
+
+#x.append(5)
+#Traceback (most recent call last):
+#  File "c:\Users\JAMES\Desktop\py4e\47th.py", line 7, in <module>
+#    x.append(5)
+#AttributeError: 'tuple' object has no attribute 'append'
+
+#x.reverse()
+#Traceback (most recent call last):
+#  File "c:\Users\JAMES\Desktop\py4e\47th.py", line 14, in <module>
+#    x.reverse()
+#AttributeError: 'tuple' object has no attribute 'reverse'
