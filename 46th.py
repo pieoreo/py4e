9cba044a749f2af44d00c14c02f4af388a797e38
pieoreo <72796740+pieoreo@users.noreply.github.com>
2022-08-22 07:40:22 +0900

    Add files via upload

---
+++ 46th.py
@@ -0,0 +1,17 @@
+x = [9, 8, 7]
+x[2] = 6
+print(x)
+
+y = 'ABC'
+#y[2] = 'D'
+#Traceback (most recent call last):
+#  File "c:\Users\JAMES\Desktop\py4e\46th.py", line 6, in <module>
+#    y[2] = 'D'
+#TypeError: 'str' object does not support item assignment"""
+
+z = (5, 4, 3)
+#z[2] = 0
+#Traceback (most recent call last):
+#  File "c:\Users\JAMES\Desktop\py4e\46th.py", line 13, in <module>
+#    z[2] = 0
+#TypeError: 'tuple' object does not support item assignment
