d139b759439aeed6f2a05801632d7b666ce0b5cb
pieoreo <72796740+pieoreo@users.noreply.github.com>
2022-10-18 13:54:37 +0900

    Create stringconnection_iteration.py
    python string connection and string iteration

---
+++ stringconnection_iteration.py
@@ -0,0 +1,11 @@
+#두 문자열을 연결하기
+connect = "ohneamamiya"+"mamiyamamiya"
+
+#반복할 변수 선언
+iterate = "Madonnadonnadonna"
+
+# 기존 변수를 이용해서 'oneamamiyamamiyamamiyaMadonnadonnadonnaMadonnadonnadonna'을 
+#변수 secret에 넣기
+madonna= (connect+iterate*2) 
+
+print(connect, iterate, madonna)
